d90226395bac98bf7477df4b60715c595cc4249d
DavidArmando00 <davidarmando0708@gmail.com>
2026-03-28 09:58:35 -0300

    questao10.py

--- questao10.py
+++ questao10.py
@@ -2,3 +2,10 @@ n1= int(input('Digite um número:'))
 n2= int(input('Digite um número:'))
 n3= int(input('Digite um número:'))
 n4= int(input('Digite um número:'))
+soma= n1+n2+n3+n4
+media=soma/4
+print(f'O valor da média é: {media}')
+if media >= 6:
+    print(f'Média maior/igual a 6. Aluno aprovado.')
+else:
+    print(f'Média menor que 6. Aluno reprovado.')
